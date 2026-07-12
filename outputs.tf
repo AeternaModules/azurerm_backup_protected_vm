9177bf5a8fac30dc780c10121f7bf070399cb089
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "backup_protected_vms" {
-  description = "All backup_protected_vm resources"
-  value       = azurerm_backup_protected_vm.backup_protected_vms
-}
 output "backup_protected_vms_backup_policy_id" {
-  description = "List of backup_policy_id values across all backup_protected_vms"
-  value       = [for k, v in azurerm_backup_protected_vm.backup_protected_vms : v.backup_policy_id]
+  description = "Map of backup_policy_id values across all backup_protected_vms, keyed the same as var.backup_protected_vms"
+  value       = { for k, v in azurerm_backup_protected_vm.backup_protected_vms : k => v.backup_policy_id }
 }
 output "backup_protected_vms_exclude_disk_luns" {
-  description = "List of exclude_disk_luns values across all backup_protected_vms"
-  value       = [for k, v in azurerm_backup_protected_vm.backup_protected_vms : v.exclude_disk_luns]
+  description = "Map of exclude_disk_luns values across all backup_protected_vms, keyed the same as var.backup_protected_vms"
+  value       = { for k, v in azurerm_backup_protected_vm.backup_protected_vms : k => v.exclude_disk_luns }
 }
 output "backup_protected_vms_include_disk_luns" {
-  description = "List of include_disk_luns values across all backup_protected_vms"
-  value       = [for k, v in azurerm_backup_protected_vm.backup_protected_vms : v.include_disk_luns]
+  description = "Map of include_disk_luns values across all backup_protected_vms, keyed the same as var.backup_protected_vms"
+  value       = { for k, v in azurerm_backup_protected_vm.backup_protected_vms : k => v.include_disk_luns }
 }
 output "backup_protected_vms_protection_state" {
-  description = "List of protection_state values across all backup_protected_vms"
-  value       = [for k, v in azurerm_backup_protected_vm.backup_protected_vms : v.protection_state]
+  description = "Map of protection_state values across all backup_protected_vms, keyed the same as var.backup_protected_vms"
+  value       = { for k, v in azurerm_backup_protected_vm.backup_protected_vms : k => v.protection_state }
 }
 output "backup_protected_vms_recovery_vault_name" {
-  description = "List of recovery_vault_name values across all backup_protected_vms"
-  value       = [for k, v in azurerm_backup_protected_vm.backup_protected_vms : v.recovery_vault_name]
+  description = "Map of recovery_vault_name values across all backup_protected_vms, keyed the same as var.backup_protected_vms"
+  value       = { for k, v in azurerm_backup_protected_vm.backup_protected_vms : k => v.recovery_vault_name }
 }
 output "backup_protected_vms_resource_group_name" {
-  description = "List of resource_group_name values across all backup_protected_vms"
-  value       = [for k, v in azurerm_backup_protected_vm.backup_protected_vms : v.resource_group_name]
+  description = "Map of resource_group_name values across all backup_protected_vms, keyed the same as var.backup_protected_vms"
+  value       = { for k, v in azurerm_backup_protected_vm.backup_protected_vms : k => v.resource_group_name }
 }
 output "backup_protected_vms_source_vm_id" {
-  description = "List of source_vm_id values across all backup_protected_vms"
-  value       = [for k, v in azurerm_backup_protected_vm.backup_protected_vms : v.source_vm_id]
+  description = "Map of source_vm_id values across all backup_protected_vms, keyed the same as var.backup_protected_vms"
+  value       = { for k, v in azurerm_backup_protected_vm.backup_protected_vms : k => v.source_vm_id }
 }
 
